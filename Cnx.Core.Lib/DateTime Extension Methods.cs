96166f0fa1d4e7cf39ecee23e0bb33c705a353a4
Thomas McNamee <rocketman.tom@gmail.com>
2018-12-12 19:04:22 -0700

    Initial Commit

---
+++ Cnx.Core.Lib/DateTime Extension Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cnx.Core.Lib
+{
+    public static class DateTime_Extension_Methods
+    {
+        static public string ISO8601(this DateTime dt)
+        {
+            return dt.ToString("yyyy-MM-ddThh:mm:ss");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/GuillaumeSalles/redux.NET
